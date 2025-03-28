cdd091c3dd8ba11aa0802d41a4c59fef39625f80
dzl-i <denzel.iskandar@gmail.com>
2025-03-29 02:44:13 +1100

    feat: initial frontend setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-5xl text-primary">GitTogether</h1>
+      <h2 className="text-2xl">Swipe. Match. Build.</h2>
+    </main>
+  );
+}
