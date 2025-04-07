467a4b2211db8bbf0d7627845e5254f15e0f79a0
dzl-i <denzel.iskandar@gmail.com>
2025-04-08 00:56:33 +1000

    feat: shadcn and lucide set up

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-5xl text-primary">GitTogether</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen gap-3">
+      <h1 className="text-5xl text-app-primary">GitTogether</h1>
       <h2 className="text-2xl">Swipe. Match. Build.</h2>
+      
+      <Button>Click Me!</Button>
+
+      <div className="flex flex-row items-center justify-center gap-4 mt-4">
+        <p>This is how to use Lucide Icons</p>
+        <PlusCircleIcon />
+      </div>
     </main>
   );
 }
