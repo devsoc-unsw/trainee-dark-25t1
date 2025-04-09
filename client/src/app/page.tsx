5094c3bd17eafbdf8844a6593c0574a451e22081
Hamsini Ganesan <hamsinig123@gmail.com>
2025-04-09 23:10:26 +1000

    Removing navbar from homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-// import { Button } from "@/components/ui/button";
-// import { PlusCircleIcon } from "lucide-react";
-import Navbar from "../components/Navbar";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
 export default function Home() {
   return (
     <main className= "flex flex-col">
-      <Navbar/>
-      {/* <h1 className="text-5xl text-app-primary">GitTogether</h1>
+      <h1 className="text-5xl text-app-primary">GitTogether</h1>
       <h2 className="text-2xl">Swipe. Match. Build.</h2>
 
       <Button>Click Me!</Button>
@@ -14,7 +12,7 @@ export default function Home() {
       <div className="flex flex-row items-center justify-center gap-4 mt-4">
         <p>This is how to use Lucide Icons</p>
         <PlusCircleIcon />
-      </div> */}
+      </div>
     </main>
   );
 }
